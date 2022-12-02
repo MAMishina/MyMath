9a97763c1ef64c440f1d100fc60ddcd66b30230e
Мария Мишина <mishinamariyaandreevna@gmail.com>
2022-12-02 20:09:29 +0300

    Добавьте файлы проекта.

---
+++ MathClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    public class MathClass
+    {
+        public string SumAB (int a, int b)
+        {
+            try
+            {
+                int sum = 0;
+                if (a > b)
+                {
+                    return "a должно быть меньше b";
+                }
+                else
+                {
+                    for (int i = a; i <= b; i++)
+                    {
+                        sum += i;
+                    }
+                    return sum.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
