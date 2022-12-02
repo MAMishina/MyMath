1a42f3ad4b59f814602ee60a1413bf642cabeb1f
Мария Андреевна Мишина <80288519+MAMishina@users.noreply.github.com>
2022-12-02 20:13:24 +0300

    Добавила файлы тестирования
    Загрузила папкой

---
+++ TestForMath/MathTest.cs
@@ -0,0 +1,21 @@
+﻿using MyMath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace TestForMath
+{
+    [TestClass]
+    public class MathTest
+    {
+        [TestMethod]
+        public void Sum_100_500_120300Returned()
+        {
+            MathClass mathClass = new MathClass();
+            string expected = "120300";
+            string actual = mathClass.SumAB(100,500);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TestForMath/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestForMath")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestForMath")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f1e533a-60a7-4818-a0ab-088d1f77d9d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
